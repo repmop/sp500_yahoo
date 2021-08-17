9c71a75969c34bca249a6f10eebd1c3e83ec7f33
Zachary B Pomper <zbp@andrew.cmu.edu>
2021-08-17 01:30:08 -0400

    Async requests should be working (never uploaded before, oops)

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-[[ ":$PATH:" != *":`python2.7 -m site --user-base`/bin:"* ]] && PATH="`python2.7 -m site --user-base`/bin:${PATH}"
-pip install virtualenv --user; export PATH="$PATH:/home/username/opt/python*/bin/python";  virtualenv venv; source venv/bin/activate; pip install  -r requirements.txt
+python3 get-pip.py --user
+[[ ":$PATH:" != *":`python3.8 -m site --user-base`/bin:"* ]] && PATH="`python3.8 -m site --user-base`/bin:${PATH}"
+pip3 install virtualenv --user; export PATH="$PATH:/home/username/opt/python*/bin/python";  virtualenv venv; source venv/bin/activate; pip install  -r requirements.txt
