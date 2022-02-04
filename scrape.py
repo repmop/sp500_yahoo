f003d78fef70c8ad41f87ffa265af1675a04a8ae
repmop <zbp@alumni.cmu.edu>
2022-02-04 10:50:04 -1000

    Remove TCP keepidle for mac

--- scrape.py
+++ scrape.py
@@ -53,7 +53,6 @@ class KeepAliveClientRequest(ClientRequest):
   async def send(self, conn: "Connection") -> "ClientResponse":
     sock = conn.protocol.transport.get_extra_info("socket")
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60)
     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 2)
     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)
     return (await super().send(conn))
